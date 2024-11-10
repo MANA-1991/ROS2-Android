4439709cad15bcb2cf8c833509ed2dcfd90d3c0a
MANA-1991 <s.m.mustafa91@gmail.com>
2024-11-10 20:10:16 +0500

    KMZ create, build and upload to Aircraft

--- app/src/main/java/jp/ac/titech/e/sc/hfg/ros2driver/ui/WPMissionFragment.kt
+++ app/src/main/java/jp/ac/titech/e/sc/hfg/ros2driver/ui/WPMissionFragment.kt
@@ -1,34 +1,46 @@
 package jp.ac.titech.e.sc.hfg.ros2driver.ui
 
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import dji.sdk.keyvalue.key.FlightControllerKey
-import dji.sdk.keyvalue.key.KeyTools
-import dji.sdk.keyvalue.value.common.EmptyMsg
-import dji.sdk.keyvalue.value.common.LocationCoordinate2D
-import dji.sdk.keyvalue.value.common.LocationCoordinate3D
-import dji.sdk.keyvalue.value.flightcontroller.GPSSignalLevel
+import com.dji.wpmzsdk.common.data.Template
+import com.dji.wpmzsdk.manager.WPMZManager
+import dji.sdk.wpmz.value.mission.WaylineMission
+import dji.sdk.wpmz.value.mission.WaylineMissionConfig
 import dji.v5.common.callback.CommonCallbacks
 import dji.v5.common.error.IDJIError
-import dji.v5.manager.KeyManager
+import dji.v5.manager.aircraft.waypoint3.WaypointMissionManager
+import dji.v5.utils.common.ContextUtil
+import dji.v5.utils.common.DiskUtil
 import dji.v5.utils.common.ThreadUtil.runOnUiThread
 import jp.ac.titech.e.sc.hfg.ros2driver.R
 import jp.ac.titech.e.sc.hfg.ros2driver.databinding.FragmentWpmissionBinding
-import jp.ac.titech.e.sc.hfg.ros2driver.ros.FC
 import jp.ac.titech.e.sc.hfg.ros2driver.ros.RosVM
+import jp.ac.titech.e.sc.hfg.ros2driver.utils.KMZTestUtil
+import jp.ac.titech.e.sc.hfg.ros2driver.utils.KMZTestUtil.createWaylineMission
+import jp.ac.titech.e.sc.hfg.ros2driver.utils.wpml.WaypointInfoModel
+import java.io.File
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
 
 class WPMissionFragment : Fragment() {
-    private val logtag = "WayPointMissionFragment"
     private val rosVM: RosVM by activityViewModels()
     private lateinit var binding: FragmentWpmissionBinding
+
+    private val WAYPOINT_SAMPLE_FILE_DIR: String = "waypoint/"
+    val rootDir = DiskUtil.getExternalCacheDirPath(ContextUtil.getContext(), WAYPOINT_SAMPLE_FILE_DIR)
+    private val showWaypoints : ArrayList<WaypointInfoModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,176 +58,56 @@ class WPMissionFragment : Fragment() {
 
         ////////////////////////////////////////////////////////////////////////////////////////////
 
-        val openPickerButton: Button = binding.root.findViewById(R.id.btnOpenPicker)
-//        val autoLandButton: Button = binding.root.findViewById(R.id.auto_land)
-//        val gpsGetButton: Button = binding.root.findViewById(R.id.gps_get)
-//        val homeSetButton: Button = binding.root.findViewById(R.id.home_set)
-//        val gotoHomeButton: Button = binding.root.findViewById(R.id.goto_home)
-
+        val saveKmzButton: Button = binding.root.findViewById(R.id.saveKmz)
+//        var showKmzfilePath:TextView = binding.root.findViewById(R.id.show_kmzfilePath)
         // Set onClick listeners
-        openPickerButton.setOnClickListener { openPickerButtonClicked() }
-//        autoLandButton.setOnClickListener { autoLandButtonClicked() }
-//        gpsGetButton.setOnClickListener { getGPSButtonClicked() }
-//        homeSetButton.setOnClickListener { setHomeLocation() }
-//        gotoHomeButton.setOnClickListener { gotoHome() }
-
-        ////////////////////////////////////////////////////////////////////////////////////////////
+        saveKmzButton.setOnClickListener { saveKMZ() }
 
         return binding.root
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private fun openPickerButtonClicked() {
+    private fun saveKMZ() {
+
+        val kmzOutPath = rootDir + "generate_test.kmz"
+        val waylineMission: WaylineMission = createWaylineMission()
+        val missionConfig: WaylineMissionConfig = KMZTestUtil.createMissionConfig()
+        val template: Template = KMZTestUtil.createTemplate(showWaypoints)
+        WPMZManager.getInstance()
+            .generateKMZFile(kmzOutPath, waylineMission, missionConfig, template)
+
         runOnUiThread {
-                    Toast.makeText(requireContext(), "Goto Home", Toast.LENGTH_LONG).show()
+                    Toast.makeText(requireContext(), "Save Kmz Success Path is : $kmzOutPath", Toast.LENGTH_LONG).show()
                 }
+        WaypointMissionManager.getInstance().pushKMZFileToAircraft(kmzOutPath, object :
+            CommonCallbacks.CompletionCallbackWithProgress<Double> {
+            override fun onProgressUpdate(progress: Double) {
+//                missionUploadState.value = MissionUploadStateInfo(updateProgress = progress)
+//                refreshMissionState()
+                runOnUiThread {
+                    Toast.makeText(requireContext(), "Uploading progress: $progress", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onSuccess() {
+//                missionUploadState.value = MissionUploadStateInfo(tips = "Mission Upload Success")
+//                refreshMissionState()
+                runOnUiThread {
+                    Toast.makeText(requireContext(), "Upload Complete", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(error: IDJIError) {
+//                missionUploadState.value = MissionUploadStateInfo(error = error)
+//                refreshMissionState()
+                runOnUiThread {
+                    Toast.makeText(requireContext(), "Error : ${error.description()}", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
     }
-//    //landCmdCallback
-//    private fun autoLandButtonClicked() {
-//        FC.landCmdCallback()
-//    }
-//    private fun gotoHome(){
-//        KeyManager.getInstance().performAction(KeyTools.createKey(FlightControllerKey.KeyStartGoHome), object: CommonCallbacks.CompletionCallbackWithParam<EmptyMsg>{
-//            override fun onSuccess(p0: EmptyMsg?) {
-//                runOnUiThread {
-//                    Toast.makeText(requireContext(), "Goto Home", Toast.LENGTH_LONG)
-//                            .show()
-//                }
-//            }
-//
-//            override fun onFailure(error: IDJIError) {
-//                runOnUiThread {
-//                    Toast.makeText(requireContext(), "Failed to set home location: ${error.description()}", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//        })
-//    }
-//    private fun setHomeLocation() {
-//        val gpsSignalKey = KeyTools.createKey(FlightControllerKey.KeyGPSSignalLevel)
-//        KeyManager.getInstance().getValue(gpsSignalKey, object : CommonCallbacks.CompletionCallbackWithParam<GPSSignalLevel> {
-//            override fun onSuccess(gpsSignalLevel: GPSSignalLevel?) {
-//                if (gpsSignalLevel != null && gpsSignalLevel != GPSSignalLevel.LEVEL_0) {
-//                    val aircraftLocationKey = KeyTools.createKey(FlightControllerKey.KeyAircraftLocation)
-//                    KeyManager.getInstance().getValue(aircraftLocationKey, object : CommonCallbacks.CompletionCallbackWithParam<LocationCoordinate2D>{
-//                        override fun onSuccess(location: LocationCoordinate2D?) {
-//                            if (location != null) {
-//                                Log.d("DJI", "Current aircraft location: Latitude = ${location.latitude}, Longitude = ${location.longitude}")
-//                                runOnUiThread {
-//                                    Toast.makeText(requireContext(), "lat:${location.latitude},lon:${location.longitude}", Toast.LENGTH_LONG)
-//                                            .show()
-//                                }
-//                                // Create a key for setting the home location
-//                                val homeLocationKey = KeyTools.createKey(FlightControllerKey.KeyHomeLocation)
-//                                KeyManager.getInstance().setValue(homeLocationKey, location, object : CommonCallbacks.CompletionCallback {
-//                                    override fun onSuccess() {
-//                                        runOnUiThread {
-//                                            Toast.makeText(requireContext(), "Home location set successfully using the current aircraft location.", Toast.LENGTH_LONG).show()
-//                                        }
-//                                        Log.d("DJI", "Home location set successfully using the current aircraft location.")
-//                                    }
-//
-//                                    override fun onFailure(error: IDJIError) {
-//                                        // Handle the failure to set home location
-//                                        runOnUiThread {
-//                                            Toast.makeText(requireContext(), "Failed to set home location: ${error.description()}", Toast.LENGTH_LONG).show()
-//                                        }
-//                                        Log.e("DJI", "Failed to set home location: ${error.description()}")
-//                                    }
-//
-//                                })
-//
-//                            }else{
-//                                // Handle failure to retrieve current location
-//                                Log.e("DJI", "Failed to retrieve current aircraft location. Location is null.")
-//                                runOnUiThread {
-//                                    Toast.makeText(requireContext(), "Failed to retrieve current aircraft location.", Toast.LENGTH_LONG).show()
-//                                }
-//                            }
-//                        }
-//
-//                        override fun onFailure(error: IDJIError) {
-//                            // Handle failure to retrieve aircraft location
-//                            Log.e("DJI", "Failed to retrieve aircraft location: ${error.description()}")
-//                            runOnUiThread {
-//                                Toast.makeText(requireContext(), "Failed to retrieve current aircraft location.", Toast.LENGTH_LONG).show()
-//                            }
-//                        }
-//                    })
-//
-//                }else{
-//                    // Handle weak GPS signal
-//                    Log.e("DJI", "GPS signal is too weak to set home location.")
-//                    runOnUiThread {
-//                        Toast.makeText(requireContext(), "GPS signal is too weak to set home location.", Toast.LENGTH_LONG).show()
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(error: IDJIError) {
-//                Log.e("DJI", "Failed to retrieve GPS signal level: ${error.description()}")
-//                runOnUiThread {
-//                    Toast.makeText(requireContext(), "Failed to retrieve GPS signal level.", Toast.LENGTH_LONG).show()
-//                }
-//            }
-//
-//        })
-//    }
-//
-//    private fun getGPSButtonClicked(){
-//        val gpsSignalKey = KeyTools.createKey(FlightControllerKey.KeyGPSSignalLevel)
-//        KeyManager.getInstance().getValue(gpsSignalKey, object : CommonCallbacks.CompletionCallbackWithParam<GPSSignalLevel> {
-//            override fun onSuccess(gpsSignalLevel: GPSSignalLevel?) {
-//                // Ensure the GPS signal level is strong enough
-//                if (gpsSignalLevel != null && gpsSignalLevel != GPSSignalLevel.LEVEL_0) {
-//                    // Create a key for setting the home location
-//                    val gpsLocationKey = KeyTools.createKey(FlightControllerKey.KeyAircraftLocation3D)
-//
-//                    KeyManager.getInstance().getValue(gpsLocationKey,  object : CommonCallbacks.CompletionCallbackWithParam<LocationCoordinate3D> {
-//                        override fun onSuccess(p0: LocationCoordinate3D?) {
-//                            p0?.let{
-//                                val latitude = it.latitude
-//                                val longitude = it.longitude
-//                                val altitude = it.altitude
-//                                runOnUiThread {
-//                                    Toast.makeText(requireContext(), "lat:$latitude,lon:$longitude,alt:$altitude", Toast.LENGTH_LONG)
-//                                            .show()
-//                                }
-//                                Log.d("DJI", "lat:$latitude,lon:$longitude,alt:$altitude")
-//
-//                            }
-//                        }
-//
-//                        override fun onFailure(error: IDJIError) {
-//                            // Handle the failure to set home location
-//                            runOnUiThread {
-//                                Toast.makeText(requireContext(), "Failed to get location.", Toast.LENGTH_LONG)
-//                                        .show()
-//                            }
-//                            Log.e("DJI", "Failed to get location: ${error.description()}")
-//                        }
-//                    })
-//                } else {
-//                    // Handle weak GPS signal
-//                    runOnUiThread {
-//                        Toast.makeText(requireContext(), "GPS signal is too weak.", Toast.LENGTH_LONG)
-//                                .show()
-//                    }
-//                    Log.e("DJI", "GPS signal is too weak.")
-//                }
-//            }
-//
-//            override fun onFailure(error: IDJIError) {
-//                // Handle failure to retrieve GPS signal level
-//                runOnUiThread {
-//                    Toast.makeText(requireContext(), "Failed to retrieve GPS signal level.", Toast.LENGTH_LONG)
-//                            .show()
-//                }
-//                Log.e("DJI", "Failed to retrieve GPS signal level: ${error.description()}")
-//            }
-//        })
-//    }
-//
-//    ////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     companion object {
         @JvmStatic
---
+++ app/src/main/java/jp/ac/titech/e/sc/hfg/ros2driver/utils/wpml/WPMLException.java
@@ -0,0 +1,8 @@
+package jp.ac.titech.e.sc.hfg.ros2driver.utils.wpml;
+
+public class WPMLException extends Exception {
+
+    public WPMLException(String message) {
+        super(message);
+    }
+}
